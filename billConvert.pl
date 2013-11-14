d20e32e71d4d1f7e811e2af7b32b7e397bade89d
kingkongmok <kingkongmok@gmail.com>
2013-11-14 10:49:21 +0800

    using sub

--- billConvert.pl
+++ billConvert.pl
@@ -21,47 +21,47 @@
 use strict;
 use warnings;
 
-    use Text::CSV;
+sub openfile {
+    open my $output , "> /home/kk/Downloads/billConvert_output.txt";
+    open FH,"< /home/kk/Downloads/bill.utf8.csv" || die "$!";
+    my @filelines = readline(FH);
+    return @filelines;
+} ## --- end sub openfile
 
-    my $file = '/home/kk/Downloads/bill.csv';
-my %hash ;
-    #my $csv = Text::CSV->new({binary=>1});
-    my $csv = Text::CSV->new();
-
-open my $output , "> /home/kk/Downloads/billConvert_output.txt";
-    open (CSV, "<:encoding(gbk)", $file) or die $!;
-    my $linecount;
-    while (<CSV>) {
-        if ($csv->parse($_)) {
-            my @columns = $csv->fields();
-            $linecount++;
-            if (  $columns[11] =~ /(\d+)\D+$/ ){
-#                print $1, "\t", $columns[6] + $columns[7],"\n" ;
-                if ( exists $hash{$1} ) {
-                    $hash{$1} = sprintf("%s\t%.2f",$hash{$1} ,  $columns[6] + $columns[7]) ;     
-                } else {
-                    $hash{$1} = $columns[6] + $columns[7] ; 
-                }
-            } else {
-                if ( $columns[2] =~ /T200P(\d+)/ ) {
-#                    print $1, "\t", $columns[6] + $columns[7],"\n" ;
-                if ( exists $hash{$1} ) {
-                    $hash{$1} = sprintf("%s\t%.2f",$hash{$1} ,  $columns[6] + $columns[7]) ;     
-                } else {
-                    $hash{$1} = $columns[6] + $columns[7] ; 
-                }
-                }
-            }
-        } else {
-            my $err = $csv->error_input;
-            print "Failed to parse line: $err";
-        }
-    }
-    close CSV;
+my @filelines = &openfile ;
 
 
-    while ( my ($k,$v) = each %hash ) {
-        print $output "$k\t$v\n" ;
-    }
 
-print $linecount ;
+#my %hash ;
+#
+#    my $linecount;
+#    while (<$fou>) {
+#        if ($csv->parse($_)) {
+#            my @columns = $csv->fields();
+#            $linecount++;
+#            if (  $columns[11] =~ /(\d+)\D+$/ ){
+##                print $1, "\t", $columns[6] + $columns[7],"\n" ;
+#                if ( exists $hash{$1} ) {
+#                    $hash{$1} = sprintf("%s\t%.2f",$hash{$1} ,  $columns[6] + $columns[7]) ;     
+#                } else {
+#                    $hash{$1} = $columns[6] + $columns[7] ; 
+#                }
+#            } else {
+#                if ( $columns[2] =~ /T200P(\d+)/ ) {
+##                    print $1, "\t", $columns[6] + $columns[7],"\n" ;
+#                if ( exists $hash{$1} ) {
+#                    $hash{$1} = sprintf("%s\t%.2f",$hash{$1} ,  $columns[6] + $columns[7]) ;     
+#                } else {
+#                    $hash{$1} = $columns[6] + $columns[7] ; 
+#                }
+#                }
+#            }
+#        } else {
+#            my $err = $csv->error_input;
+#            print "Failed to parse line: $err";
+#        }
+#    }
+#    close CSV;
+#
+#
+#
