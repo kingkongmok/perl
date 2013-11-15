56d42a7268b9c44dc606215d0a7287d860005777
kingkongmok <kingkongmok@gmail.com>
2013-11-15 14:25:43 +0800

    billConvert.pl hoh done

--- billConvert.pl
+++ billConvert.pl
@@ -22,25 +22,80 @@ use strict;
 use warnings;
 use Text::CSV;
 
-my $file = "/home/kk/Downloads/bill.utf8.csv" || die "$!";
+#my $file = "/home/kk/Downloads/bill.utf8.csv" || die "$!";
 #my $csv = Text::CSV->new ({ binary => 1, });
-my $csv = Text::CSV->new({ binary => 1, });
-open my $io, "<", $file or die "$file: $!";
+my $csv = Text::CSV->new({binary=>1});
+#open my $io, "<", $file or die "$file: $!";
 
-#while (my $row = $csv->getline($io)) {
-#    my @fields = @$row;
-#    print @fields ;
-#}
+open FH, "< /home/kk/Downloads/bill.utf8.csv" || die "$!" ;
+my@file=readline(FH);
 
-while ( <$io> ) {
+#open OUT, "> /tmp/test.file";
+#my @catalogs = ("天猫佣金（类目）",);
+
+my %result;
+my %ids;
+
+my %catalogs = (
+"T200P"=>"提现",
+#"AS:收入金额（+元）";
+#"售后维权(-) AS售后维权：天猫佣金"
+"售后维权"=>"售后维权(-) AS售后维权：天猫佣金",
+#"售后维权：代扣交易退回积分"
+#"代扣返点积分"
+#"代扣款（扣款用途：天猫佣金（类目）"=>"淘宝客佣金代扣款",
+"天猫佣金"=>"淘宝客佣金代扣款",
+#"天猫佣金（类目）"
+#"线上支付服务费——信用卡快捷支付"
+"代扣返点积分"=>"代扣交易退回积分",
+);
+
+foreach my $catalog ( keys %catalogs ) {
+
+for ( @file ) {
+    my $targetString;
+    my $id;
     $_ =~ s/(?<=[^"])(\,)/",/g;
     $_ =~ s/(\,)(?=[^"])/,"/g;
-    if ($csv->parse($_)) {
-        my @columns = $csv->fields();
-        if (  $columns[11] =~ /(\d+)\D+$/ ){
-            #print $1, "\t", $columns[6] + $columns[7],"\n" ;
-            print $columns[11] , "\n";
+    $_ =~ s/^"//;
+    $_ =~ s/"$//;
+    my @F = split/","/;
+    if ( $F[11] ) {
+        if ( $F[11] =~ /(\d+)\D+$/ ) {
+            $ids{$catalog}=$1;
+            $result{$ids{$catalog}}=$F[7];
+            
         }
+        $targetString=$F[11];
+    }
+    else {
+        $targetString=$F[2]; 
+    }
+    if ( $targetString =~ /$catalog/ ) {
+        $result{$catalogs{$catalog}}++ ;
     }
 }
+
+#-------------------------------------------------------------------------------
+#  get id
+#-------------------------------------------------------------------------------
+    
+#
+#    if ($csv->parse($_)) {
+#        my @columns = $csv->fields();
+#
+#        print @columns ;
+##        foreach my $catalog ( @catalogs ) {
+##            if ( $columns[11] =~ /\d/ ) {
+##                print $columns[11] ;
+###                if (  $columns[11] =~ /(\d+)\D+$/ ){
+###                    #print $1, "\t", $columns[6] + $columns[7],"\n" ;
+###                    print $columns[11] , "\n";
+###                }
+##            }
+##        }
+#    }
+}
+
+
 
