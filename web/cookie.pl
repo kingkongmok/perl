f984de20bf1ea7f0f26f93040d7ddd2dcb427541
kingkongmok <kingkongmok@gmail.com>
2013-11-22 17:32:45 +0800

    session.pl

--- web/cookie.pl
+++ web/cookie.pl
@@ -14,44 +14,95 @@
 #       AUTHOR: Kingkong Mok (), kingkongmok AT gmail DOT com
 #      COMPANY: 
 #      VERSION: 1.0
-#      CREATED: 10/23/2013 12:14:25 PM
+#      CREATED: 10/23/2013 02:48:20 PM
 #     REVISION: ---
 #===============================================================================
 
-#use strict;
-#use warnings;
-use Storable qw/freeze/;
-use CGI::Carp qw/fatalsToBrowser/;
+use strict;
+use warnings;
 
 use CGI;
-use CGI::Cookie;
-
-my %prefs = (
-size=>"big",
-colors=>"red",
-address=>"guangzhou",
-);
-
-my $c = CGI::Cookie->new(
--name=>"kk",
--value=>\%prefs,
-#-expires=>"+1d",
-#-domain=>".kk.igb",
-#-path=>"/",
-#-secure=>"0",
-);
-
-my $cookiefetch = CGI::Cookie->fetch;
+use CGI::Carp qw/fatalsToBrowser/;
+use CGI::Cookie ;
 
 my $q = new CGI;
 
-if ( $cookiefetch ) {
-    print $q->header();
-    while ( my($k,$v)=each$cookiefetch ) {
-        print "$k" ;
-        print "\nyou like $v\n\n" ;
+if ( $q->param("cookie") ) {
+    &setCookies;
+}
+else {
+    if ( my %cookies=CGI::Cookie->fetch ) {
+        if (&checkCookie(\%cookies)){
+            &showCookiePref(\%cookies) ;
+        }
     }
-} else {
-    print $q->header(-cookie=> $c);
+    else {
+        &printHeaderCookie ;
+    }
 }
 
+sub printHeaderCookie {
+    my $cookie1 = CGI::Cookie->new(-name=>'ID',-value=>123456);
+    my $cookie2 = CGI::Cookie->new( -name=>'preferences', 
+        -value=>{ 
+            font => "Helvetica",
+             size => 12 ,
+         }  );
+    print $q->header(-cookie=>[$cookie1,$cookie2]), 
+        "hello stranger, please enable cookies.\n";
+    return 0;
+}
+
+sub checkCookie {
+    if (my	%cookies = %{shift()} ){ 
+        my $id = $cookies{ID}->value ;
+        return 1 if $id == '123456' ;
+    }
+    return 0;
+} ## --- end sub checkCookie
+
+
+sub showCookiePref {
+    if (my  %cookies = %{shift()} ){
+        my%hash = $cookies{preferences}->value;
+        print $q->header; 
+        while ( my($k,$v)=each%hash ) {
+            print "you\'re $k is $v\n" ;
+        }
+    }
+    return 1;
+} ## --- end sub showCookiePref
+
+
+sub setCookies {
+    my @names = $q->param ;
+    my %cookie;
+    foreach  (@names) {
+        $cookie{$_}=$q->param($_);
+    }
+    my $cookies = CGI::Cookie->new( -name=>"name",
+    -value=>\%cookie);
+        print $q->header(-cookie=>$cookies);;
+    return 0;
+} ## --- end sub setCookies
+
+#sub showDemoCookie {
+#    print $q->header, "hello";
+#    my%cookies = %{shift()};
+#    my $cookie1 = CGI::Cookie->new(-name=>'ID',-value=>123456);
+#    my $cookie2 = CGI::Cookie->new( -name=>'preferences', 
+#        -value=>{ 
+#            font => "Helvetica",
+#             size => 12 ,
+#         }  );
+#    my$id = $cookies{'ID'}->value;
+#    my%hash = $cookies{'preferences'}->value;
+#    print $q->header(-cookie=>[$cookie1,$cookie2]),
+#    "you had cookie\n",
+#    "the demo cookie ID is $id\n",
+#    "and the demo cookie is below\n",
+#    while ( my($k,$v)=each%hash ) {
+#        "$k\t$v\n" ;
+#    }
+#    return 1;
+#}
