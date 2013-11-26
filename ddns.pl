da9bce9aa81df8929cbbdea952e4e2194605d20f
kingkongmok <kingkongmok@gmail.com>
2013-11-26 10:12:28 +0800

    ddns.pl

---
+++ ddns.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+#===============================================================================
+#
+#         FILE: ddns.pl
+#
+#        USAGE: ./ddns.pl  
+#
+#  DESCRIPTION: 
+#
+#      OPTIONS: ---
+# REQUIREMENTS: ---
+#         BUGS: ---
+#        NOTES: ---
+#       AUTHOR: Kingkong Mok (), kingkongmok AT gmail DOT com
+#      COMPANY: 
+#      VERSION: 1.0
+#      CREATED: 11/26/2013 10:03:56 AM
+#     REVISION: ---
+#===============================================================================
+
+use strict;
+use warnings;
+
+use password ;
+
+
+
+#curl "http://kingkongmok:whatisup@members.3322.org/dyndns/update?system=dyndns&hostname=kingkongmok.3322.org&mx=kingkongmok.3322.org"
