fbf59993a295c372c40e571b4452360d7a511736
kingkongmok <kingkongmok@gmail.com>
2013-12-02 16:06:30 +0800

    change single quota to #

--- routerReboot.pl
+++ routerReboot.pl
@@ -26,10 +26,10 @@ use password ;
 my%password=&getpassword;
 
 
-if ( system('curl -q "http://$password{kk}{username}:$password{kk}{password}\@192.168.1.1/userRpm/StatusRpm.htm?Disconnect=%B6%CF%20%CF%DF&wan=1" >/dev/null 2>&1') ) {
+if ( qx#curl -q "http://$password{kk}{username}:$password{kk}{password}\@192.168.1.1/userRpm/StatusRpm.htm?Disconnect=%B6%CF%20%CF%DF&wan=1" >/dev/null 2>&1# ) {
 }
 
-if (system('curl -q "http://$password{kk}{username}:$password{kk}{password}\@192.168.1.1/userRpm/StatusRpm.htm?Connect=%C1%AC%20%BD%D3&wan=1" >/dev/null 2>&1') ) {
+if ( qx#curl -q "http://$password{kk}{username}:$password{kk}{password}\@192.168.1.1/userRpm/StatusRpm.htm?Connect=%C1%AC%20%BD%D3&wan=1" >/dev/null 2>&1# ) {
 }
 
 my $ping ;
@@ -41,6 +41,6 @@ do {
 
 my $update;
 do {
-    $update=system('curl -q "http://$password{us}{username}:$password{kk}{password}\@members.3322.org/dyndns/update?system=dyndns&hostname=$password{us}{username}.3322.org&mx=$password{us}{username}.3322.org" > /dev/null 2>&1');
+    $update=qx#curl -q "http://$password{us}{username}:$password{kk}{password}\@members.3322.org/dyndns/update?system=dyndns&hostname=$password{us}{username}.3322.org&mx=$password{us}{username}.3322.org" > /dev/null 2>&1#;
     sleep 1 ;
 } while ( $update );				# -----  end do-while  -----
