3e134e8d8439cf400126409611d44d4d4b292ca1
kingkongmok <kingkongmok@gmail.com>
2013-12-18 10:04:40 +0800

    mlcupload.sh finished, to upload mlocate db

---
+++ mlcupload.sh
@@ -0,0 +1,50 @@
+x!/bin/bash - 
+#===============================================================================
+#
+#          FILE: mlcupload.sh
+# 
+#         USAGE: ./mlcupload.sh 
+# 
+#   DESCRIPTION: use gpg encrypt the mlocate.db and put it into dropbox.
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: kk (Kingkong Mok), kingkongmok AT gmail DOT com
+#  ORGANIZATION: 
+#       CREATED: 12/18/2013 08:57:10 AM CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+[ -r /etc/default/locale ] && . /etc/default/locale
+[ -n "$LANG" ] && export LANG
+
+LOCATEFILE=/var/lib/mlocate/mlocate.db
+DROPXBOXLOCATE=
+DOCLOCATION=/home/kk/Documents/personal
+DOCFILE=/home/kk/Documents/personal/mlocate.db
+DOCGPGFILE=/home/kk/Documents/personal/mlocate.db.gpg
+
+
+#if [ ! "$(id $USER | grep mlocate)" ] ; then
+#    echo "please add user to the locate group."
+#    exit 73 ;
+#fi
+
+if [ "$(ps -ef | grep [u]pdatedb)" ] ; then
+    echo "mlodatedb uplodateing please wait." ;    
+    exit 74;
+fi
+
+
+if [ -d $DOCLOCATION ] ; then
+    sudo cp -a $LOCATEFILE $DOCLOCATION &&\
+    sudo chown kk:kk $DOCFILE &&\
+    gpg -e -r kingkongmok@gmail.com $DOCFILE &&\
+    shred -u $DOCFILE &&\
+    mv $DOCGPGFILE ~/Dropbox/Documents/personal/
+fi
+
+
