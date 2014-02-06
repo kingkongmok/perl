14c6f47a8c8ef04db3364f58f752457c3dd1d45b
kingkongmok <kingkongmok@gmail.com>
2014-02-06 11:45:51 +0800

    showDownload.sh to show mldonkey/mlnet download status using perl printf

---
+++ showDownload.sh
@@ -0,0 +1,25 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: showDownload.sh
+# 
+#         USAGE: ./showDownload.sh 
+# 
+#   DESCRIPTION: show mldonkey donwload status;
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: kk (Kingkong Mok), kingkongmok AT gmail DOT com
+#  ORGANIZATION: 
+#       CREATED: 02/06/2014 11:21:23 AM CST
+#      REVISION:  ---
+#===============================================================================
+
+set -o nounset                              # Treat unset variables as an error
+[ -r /etc/default/locale ] && . /etc/default/locale
+[ -n "$LANG" ] && export LANG
+
+
+echo vd | nc -q 1 localhost 4040 | perl -lane 'printf "%3s%5s%%%6s\t%s\n",@F[1,$#F-7,$#F-1,6..$#F-8,] if /\[B/' | sort -n
