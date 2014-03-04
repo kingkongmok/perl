f71a8df993e46d8c45773f7afb8e4a33c7aa93a4
kingkongmok <kingkongmok@gmail.com>
2014-03-04 14:16:55 +0800

    add view, insert, funtion.

--- dbiview_test.pl
+++ dbiview_test.pl
@@ -25,11 +25,52 @@ use DBI;
 my $dsn = "dbi:mysql:test" ;
 my $user = "kk" ;
 my $password = "kk" ;
-my $dbh = DBI->connect($dsn, $user, $password) || die "connect error";
 
-my $sth = $dbh->prepare("SELECT * from user") || die "prepare error";
-$sth->execute ;
+my $simpleQueryName = "testname";
 
-while ( my @row = $sth->fetchrow_array ) {
-print "@row\n";
-}
+
+my $dbh = connectDBI($dsn, $user, $password);
+
+#&simpleQuery($dbh, $simpleQueryName );
+#my %result = &simpleQueryhash($dbh, $simpleQueryName );
+
+my @insertStrings = ( "name1", "f", "2010-1-10" );
+&simpleInsert($dbh, @insertStrings) ;
+
+$dbh->disconnect() ;
+
+sub simpleInsert {
+    my $dbh = shift ;
+    my	( $name, $sex, $birth )	= @_;
+    my $sql = "INSERT INTO user(name, sex, birth) values(?,?,?)";
+    my $sth = $dbh->prepare($sql) ;
+    $sth->execute($name, $sex, $birth) ;
+
+#    $dbh->do('INSERT INTO user VALUES('$name', '$sex', '$birth' )') || die $!;
+
+    return ;
+} ## --- end sub simpleInsert
+
+sub simpleQueryhash {
+    my	( $dbh, $simpleQueryName )	= @_;
+    my $sth = $dbh->prepare("SELECT * FROM user WHERE name='$simpleQueryName' ") || die "prepare error";
+    $sth->execute ;
+    my $result =  $sth->fetchrow_hashref;
+    return %$result ;
+} ## --- end sub simpleQueryhash
+
+sub simpleQuery {
+    my	( $dbh, $simpleQueryName )	= @_;
+    my $sth = $dbh->prepare("SELECT * FROM user WHERE name='$simpleQueryName' ") || die "prepare error";
+    $sth->execute ;
+    my @results =  $sth->fetchrow_array;
+    use Data::Dumper;
+    print Dumper(\@results);
+} ## --- end sub simpleQuery
+
+sub connectDBI {
+    my	( $dsn, $sqluser, $sqlpassword )	= @_;
+    my $dbh = DBI->connect($dsn, $user, $password, {RaiseError => 1}) || die "connect error";
+    return $dbh;
+} ## --- end sub connectDBI
+
