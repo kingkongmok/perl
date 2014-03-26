37dfdd81701f0023dbe0065a54ac927092c115fe
kingkongmok <kingkongmok@gmail.com>
2014-03-26 11:15:09 +0800

    MyScript.pl should not be changed

--- MyScript.pl
+++ MyScript.pl
@@ -31,9 +31,9 @@ my @list = qw (J u s t ~ A n o t h e r ~ P e r l ~ H a c k e r !);
 # print func2(@list),"\n";
 
 # case 2
- use MyModule qw(&func1);
- print func1(@list),"\n";
- print MyModule::func2(@list),"\n";
+# use MyModule qw(&func1);
+# print func1(@list),"\n";
+# print MyModule::func2(@list),"\n";
 
 # case 3
 # use MyModule qw(:DEFAULT);
@@ -44,5 +44,3 @@ my @list = qw (J u s t ~ A n o t h e r ~ P e r l ~ H a c k e r !);
 # use MyModule qw(:Both);
 # print func1(@list),"\n";
 # print func2(@list),"\n";
-
-
